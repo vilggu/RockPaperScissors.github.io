22bc9a3ecdf0c287280fa585b348beb7ba6d1f66
Viljami Männikkö <viljami.mannikko@student.tut.fi>
2018-04-04 13:14:03 +0300

    pelin lisäys

---
+++ RockPaperScissors.js
@@ -0,0 +1,157 @@
+
+$(document).ready( function() {
+  function prepare(){
+    readyToChoose = false;
+    setTimeout(
+      function(){
+        readyToChoose = true;
+        $("#commandText").text("Choose your hand:");
+        $("#paper").css("border","4px solid white");
+        $("#rock").css("border","4px solid white");
+        $("#scissors").css("border","4px solid white");
+      },1000);
+  }
+
+  function getComputerHand(){
+    var randomNumber = Math.floor(Math.random()*3);
+    var computerHand = "";
+    if(randomNumber == 0){
+        computerHand = "rock";
+    }
+    if(randomNumber == 1){
+        computerHand = "paper";
+    }
+    if(randomNumber == 2){
+        computerHand = "scissors";
+    }
+    return computerHand;
+  }
+
+  function gameOver(){
+    readyToChoose = false;
+    $("#commandText").text("Game Over!");
+    var msg = {
+      "messageType": "SCORE",
+      "score": parseFloat($("#points").text())
+    };
+    points = 0;
+    $("#points").text(points);
+    window.parent.postMessage(msg, "*");
+
+  }
+
+  "use strict";
+  var points = 0;
+  var readyToChoose = true;
+  console.log(readyToChoose);
+  $("#save").click(function(){
+    var msg = {
+      "messageType": "SAVE",
+      "score": parseFloat($("#points").text())
+    };
+    window.parent.postMessage(msg, "*");
+    console.log(msg);
+  });
+
+  $("#load").click(function(){
+    var msg = {
+      "messageType": "LOAD_REQUEST",
+    };
+    window.parent.postMessage(msg, "*");
+  });
+
+
+  $("#newgame").click(function(){
+    readyToChoose = true;
+    points = 0;
+    $("#commandText").text("Choose your hand:");
+    $("#paper").css("border","4px solid white");
+    $("#rock").css("border","4px solid white");
+    $("#scissors").css("border","4px solid white");
+    $("#points").text(points);
+  });
+
+  $("#rock").click(function(){
+    if(readyToChoose===true){
+      $("#rock").css("border","4px solid yellow");
+      var computerHand = getComputerHand();
+      var tulosTeksti = "";
+      if(computerHand == "paper"){
+        $("#paper").css("border","4px solid red");
+        tulosTeksti = "You lost!";
+        gameOver();
+      }
+      if(computerHand == "rock"){
+        $("#rock").css("border","4px solid red");
+        tulosTeksti = "Draw!";
+        $("#commandText").text(tulosTeksti);
+        $("#points").text(points);
+        prepare();
+      }
+      if(computerHand == "scissors"){
+        $("#scissors").css("border","4px solid red");
+        tulosTeksti = "You win!";
+        points = points+1;
+        $("#commandText").text(tulosTeksti);
+        $("#points").text(points);
+        prepare();
+      }
+    }
+  });
+
+  $("#paper").click(function(){
+    if(readyToChoose === true){
+      var computerHand = getComputerHand();
+      $("#paper").css("border","4px solid yellow");
+      var tulosTeksti = "";
+      if(computerHand == "paper"){
+        $("#paper").css("border","4px solid red");
+        tulosTeksti = "Draw!";
+        $("#commandText").text(tulosTeksti);
+        $("#points").text(points);
+        prepare();
+      }
+      if(computerHand == "rock"){
+        $("#rock").css("border","4px solid red");
+        tulosTeksti = "You win!";
+        points = points+1;
+        $("#commandText").text(tulosTeksti);
+        $("#points").text(points);
+        prepare();
+      }
+      if(computerHand == "scissors"){
+        $("#scissors").css("border","4px solid red");
+        tulosTeksti = "You lost!";
+        gameOver();
+      }
+    }
+  });
+
+  $("#scissors").click(function(){
+    if(readyToChoose === true){
+      $("#scissors").css("border","4px solid yellow");
+      var computerHand = getComputerHand();
+      var tulosTeksti = "";
+      if(computerHand == "paper"){
+        $("#paper").css("border","4px solid red");
+        tulosTeksti = "You win!";
+        points = points+1;
+        $("#commandText").text(tulosTeksti);
+        $("#points").text(points);
+        prepare();
+      }
+      if(computerHand == "rock"){
+        $("#rock").css("border","4px solid red");
+        tulosTeksti = "You lost!";
+        gameOver();
+      }
+      if(computerHand == "scissors"){
+        $("#scissors").css("border","4px solid red");
+        tulosTeksti = "Draw!";
+        $("#commandText").text(tulosTeksti);
+        $("#points").text(points);
+        prepare();
+      }
+    }
+  });
+});
